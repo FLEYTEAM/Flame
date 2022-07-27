c526a7f5d3599e76a990bd82e1e82b758d68c54e
Viet <fleyteam@gmail.com>
2022-07-27 10:19:58 +0700

    Update 7/27/2002 10:20 v2

--- dist/js/base.js
+++ dist/js/base.js
@@ -1,6 +1,9 @@
 const DROPDOWN_NAME = 'dropdown';
 const DROPDOWN_SHOW = 'show';
 
+const MODAL_NAME = 'modal';
+const MODAL_SHOW = 'show';
+
 // Create selector
 var flame = function(selector) {
     if (! (this instanceof flame) ) {
@@ -15,19 +18,41 @@ var flame = function(selector) {
     dropdown: {
         name: DROPDOWN_NAME,
         show: DROPDOWN_SHOW
+    },
+    modal: {
+        name: MODAL_NAME,
+        show: MODAL_SHOW,
     }
 };
 
+// Toggle function
+function __classToggle(a, element, type) {
+    let __elementToggleState = element.classList.contains(window.autoClose[type].show);
+    if(a == 'hide') {
+        if(__elementToggleState) {
+            element.classList.remove(window.autoClose[type].show);
+            autoCloseQueue = autoCloseQueue.filter(function(item) { return item !== window.autoClose[type].name });
+        }
+    }
+    if(a == 'show') {
+        if(!__elementToggleState) {
+            element.classList.add(window.autoClose[type].show);
+            !autoCloseQueue.includes(window.autoClose[type].name) ? autoCloseQueue.push(window.autoClose[type].name) : '';
+        }
+    }
+}
+
 var autoCloseQueue = [];
+
+const __DROPDOWN_ELEMENTS = flame('.' + DROPDOWN_NAME).elements;
 
 document.addEventListener('click', function(e) {
     console.log(autoCloseQueue);
     for(i in autoCloseQueue) {
-        // Dropdown auto close
-        if(autoCloseQueue[i] === autoClose[[autoCloseQueue[i]]].name) {
-            if (!e.target.className.includes(autoClose[[autoCloseQueue[i]]].name)) {
-                flame('.' + DROPDOWN_NAME + '.' + DROPDOWN_SHOW).dropdown('hide');
-            }
+        // Auto close
+        if(autoCloseQueue[i] === window.autoClose[[autoCloseQueue[i]]].name) {
+            let autoCloseQuery = '.' + window.autoClose[[autoCloseQueue[i]]].name + '.' + window.autoClose[[autoCloseQueue[i]]].show;
+            flame(autoCloseQuery).dropdown('hide');
         }
     }
 });
--- dist/js/dropdown.js
+++ dist/js/dropdown.js
@@ -1,34 +1,21 @@
-function __dropdownToggle(a, element) {
-    let __dropdownState = element.classList.contains(DROPDOWN_SHOW);
-    if(a == 'hide') {
-        if(__dropdownState) {
-            element.classList.remove(DROPDOWN_SHOW);
-            autoCloseQueue = autoCloseQueue.filter(function(item) { return item !== DROPDOWN_NAME });
-        }
-    }
-    if(a == 'show') {
-        if(!__dropdownState) {
-            element.classList.add(DROPDOWN_SHOW);
-            !autoCloseQueue.includes(DROPDOWN_NAME) ? autoCloseQueue.push(DROPDOWN_NAME) : '';
-        }
-    }
-}
-flame.prototype.dropdown = function(e){
+flame.prototype.dropdown = function(e) {
     this.elements.forEach(function(element) {
-        __dropdownToggle(e, element);
+        __classToggle(e, element, 'dropdown');
     });
 }
 
-const __DROPDOWN_ELEMENTS = flame('.' + DROPDOWN_NAME).elements;
-
-__DROPDOWN_ELEMENTS.forEach(function(element) {
-    let toggleElement = element.querySelector('[data-toggle="' + DROPDOWN_NAME + '"]');
-    if(toggleElement) {
-        toggleElement.addEventListener('click', function(e){
+if(__DROPDOWN_ELEMENTS) {
+    for(var i = 0; i < __DROPDOWN_ELEMENTS.length; i++) {
+        // __DROPDOWN_ELEMENTS[i].setAttribute('data-dropdown-id', i);
+        __DROPDOWN_ELEMENTS[i].addEventListener('click', function(e){
             e.stopPropagation();
-            window._dropdownClickedElement = element;
-            let __dropdownAction = element.classList.contains(DROPDOWN_SHOW) ? 'hide' : 'show';
-            __dropdownToggle(__dropdownAction, element);
+            window._dropdownClickedElement = this;
+            let __dropdownAction = this.classList.contains(DROPDOWN_SHOW) ? 'hide' : 'show';
+            // Close all the opened
+            for(var j = 0; j < __DROPDOWN_ELEMENTS.length; j++) {
+                __classToggle('hide', __DROPDOWN_ELEMENTS[j], 'dropdown');
+            }
+            __classToggle(__dropdownAction, this, 'dropdown');
         });
     }
-});
+}
